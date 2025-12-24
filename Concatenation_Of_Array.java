31b487041e182ce23136a626fb8bc7f99e7cf43e
Rahman M S <msr1707rich@gmail.com>
2025-12-24 21:14:44 +0530

    Simplify array concatenation logic

--- Concatenation_Of_Array.java
+++ Concatenation_Of_Array.java
@@ -6,12 +6,9 @@ class Solution {
     public int[] getConcatenation(int[] nums) {
         int n = nums.length + nums.length;
         int[] ans = new int[n];
-
-        for (int i = 0; i < nums.length; i++) {
+        for(int i = 0 ; i < nums.length ; i++){
             ans[i] = nums[i];
-            for (int j = i + nums.length; j < n; j++) {
-                ans[j] = nums[i];
-            }
+            ans[i + nums.length] = nums[i];
         }
         return ans;
     }
